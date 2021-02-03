0d8640093b7dd6c06c0aa86abebeb1b5c76563bf
arud95 <antonrud95@yandex.ru>
2021-02-03 13:25:39 +0200

    wip: animate burger

--- src/components/ui/Navbar/Navbar.js
+++ src/components/ui/Navbar/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../Logo/Logo';
 import {Container, Navbar, Nav} from 'react-bootstrap';
-import classnames from 'classnames'
+// import classnames from 'classnames'
 
 import styles from './navbar.module.scss'
 
 const SNavbar = () => {
+    const [click, setClick] = useState(false)
+    const clickedBurger = () => {
+        setClick(!click)
+    }
     return(
         <Navbar fixed="top" className={styles.navbar}>
             <Container>
                 <Logo />
-                <Nav className={styles.burgerMenu}>
+                <Nav className={styles.burgerMenu} onClick={clickedBurger}>
                     <span className={styles.burgerLine}/>
-                    <span className={classnames(styles.burgerLine, styles.centerLine)}/>
+                    <span className={click ? styles.centerLine : styles.burgerLine}/>
                     <span className={styles.burgerLine}/>
                 </Nav>
             </Container>
