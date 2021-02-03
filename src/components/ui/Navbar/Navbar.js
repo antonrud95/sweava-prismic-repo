3c69750d567ff7892abd99904bc6aaf14bdfb3ff
arud95 <antonrud95@yandex.ru>
2021-02-03 14:53:40 +0200

    feature: add navbar

--- src/components/ui/Navbar/Navbar.js
+++ src/components/ui/Navbar/Navbar.js
@@ -28,6 +28,22 @@ const SNavbar = () => {
                         Download app
                     </Button>
                 </Nav>
+                <Nav className={click ? classnames(styles.mobileMenu, styles.mobileMenu__active) : styles.mobileMenu}>
+                    <div className={styles.linksWrapper}>
+                        <Link to="/" className={styles.navLink}>Home</Link>
+                        <Link to="/" className={styles.navLink}>Team</Link>
+                        <Link to="/" className={styles.navLink}>Faq</Link>
+                        <Link to="/" className={styles.navLink}>Contact</Link>
+                    </div>
+                    <div>
+                        <Button
+                            variant="primary"
+                            additionalClasses={styles.navButton}
+                        >
+                            Download app
+                        </Button>
+                    </div>
+                </Nav>
                 <Nav className={styles.burgerMenu} onClick={clickedBurger}>
                     <span className={!click ? styles.burgerLine : classnames(styles.burgerLine, styles.burgerLine__first)}/>
                     <span className={click ? styles.centerLine : styles.burgerLine}/>
