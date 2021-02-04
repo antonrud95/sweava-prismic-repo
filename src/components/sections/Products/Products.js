441ceae6ec24597f362ba562bf2152dca5547e80
arud95 <antonrud95@yandex.ru>
2021-02-04 23:03:58 +0200

    feature: add products section

--- src/components/sections/Products/Products.js
+++ src/components/sections/Products/Products.js
@@ -8,15 +8,14 @@ const Products = ({title, description, image}) => {
     return(
         <Container className={styles.productsWrapper}>
             <Row className={styles.row}>
-                <Col xs="12" md="6">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
+                <Col xs="12" md="6" className={styles.contentWrapper}>
+                    <h2 className={styles.productsTitle}>{title}</h2>
+                    <p className={styles.productsText}>{description}</p>
                 </Col>
                 <Col xs="12" md="6" className={styles.imageWrapper}>
                     <img src={image} alt="products" className={styles.image}/>
                 </Col>
             </Row>
-            Products section
         </Container>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,33 +15,33 @@ const IndexPage = ({data}) => {
   return (
     <Layout>
       <Hero 
-        title={data.allPrismicInfoSection.nodes[5].data.title[0].text} 
-        text={data.allPrismicInfoSection.nodes[5].data.description[0].text} 
-        image={data.allPrismicInfoSection.nodes[5].data.picture.url}
-        button={data.allPrismicInfoSection.nodes[5].data.button[0].text}
+        title={data.allPrismicInfoSection.nodes[0].data.title[0].text} 
+        text={data.allPrismicInfoSection.nodes[0].data.description[0].text} 
+        image={data.allPrismicInfoSection.nodes[0].data.picture.url}
+        button={data.allPrismicInfoSection.nodes[0].data.button[0].text}
       />
       <Account 
-        sectionTitle={data.allPrismicInfoSection.nodes[2].data.sectiontitle[0].text}
-        upperTitle={data.allPrismicInfoSection.nodes[2].data.uppertitle[0].text}
-        title={data.allPrismicInfoSection.nodes[2].data.title[0].text}
-        text={data.allPrismicInfoSection.nodes[2].data.description[0].text}
-        image={data.allPrismicInfoSection.nodes[2].data.picture.url}
-        mobileimage={data.allPrismicInfoSection.nodes[2].data.mobilepicture.url}
+        sectionTitle={data.allPrismicInfoSection.nodes[1].data.sectiontitle[0].text}
+        upperTitle={data.allPrismicInfoSection.nodes[1].data.uppertitle[0].text}
+        title={data.allPrismicInfoSection.nodes[1].data.title[0].text}
+        text={data.allPrismicInfoSection.nodes[1].data.description[0].text}
+        image={data.allPrismicInfoSection.nodes[1].data.picture.url}
+        mobileimage={data.allPrismicInfoSection.nodes[1].data.mobilepicture.url}
       />
       <Products 
-        title={data.allPrismicInfoSection.nodes[3].data.title[0].text}
-        description={data.allPrismicInfoSection.nodes[3].data.description[0].text}
-        image={data.allPrismicInfoSection.nodes[3].data.picture.url}
+        title={data.allPrismicInfoSection.nodes[2].data.title[0].text}
+        description={data.allPrismicInfoSection.nodes[2].data.description[0].text}
+        image={data.allPrismicInfoSection.nodes[2].data.picture.url}
       />
     </Layout>
   )
 }
 export const query = graphql`
   query {
-    allPrismicInfoSection {
+    allPrismicInfoSection(sort: {fields: data___numeration}) {
       nodes {
         data {
-          id
+          numeration
           uppertitle {
             text
           }
